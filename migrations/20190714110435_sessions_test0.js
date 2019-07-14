4ad1b855215347e1cf68fb5ae1daf3abe07b6e9e
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-07-14 11:12:13 -0700

    Set up column names in my table

--- migrations/20190714110435_sessions_test0.js
+++ migrations/20190714110435_sessions_test0.js
@@ -1,8 +1,26 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('sessions', sessions => {
+    
+    // Primary key   
+    sessions.increments(); 
+ 
+    // Country name 
+    sessions 
+        .string('country', 100)
+        .notNullable()
+        .unique(); 
+
+    // Product name 
+    sessions
+        .string('product', 100)
+        .notNullable(); 
+    
+    // Timestamp 
+    sessions.timestamps(); 
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('sessions'); 
 };

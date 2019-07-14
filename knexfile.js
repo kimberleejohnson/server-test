eeeed45753456e227b5dcb5d4fed303630ee67b3
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-07-14 11:02:55 -0700

    Updated knexfile to user mysql

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.01',
+      user: 'root', 
+      password: '', 
+      database: 'sauti_test0',
+      charset: 'utf8'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.01',
+      user: 'root', 
+      password: '', 
+      database: 'sauti_test0',
+      charset: 'utf8'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.01',
+      user: 'root', 
+      password: '', 
+      database: 'sauti_test0',
+      charset: 'utf8'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
